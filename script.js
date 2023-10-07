bf53db3d8105546f254f174e8b51c1d83564ebe4
oleks211 <aleksandr.savelyev@gmail.com>
2023-10-07 20:41:31 +0300

    complete level-100

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+function formatEpisodeCode(season, episode) {
+  return `S${season.toString().padStart(2, "0")}E${episode
+    .toString()
+    .padStart(2, "0")}`;
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function setup() {
+  const rootDiv = document.getElementById("root");
+  const allEpisodes = getAllEpisodes();
+
+  allEpisodes.forEach((episode) => {
+    const episodeCode = formatEpisodeCode(episode.season, episode.number);
+    const episodeName = episode.name;
+    const episodeImage = episode.image.medium;
+    const episodeSummary = episode.summary;
+
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+      <h3>${episodeCode}: ${episodeName}</h3>
+      <img src="${episodeImage}" alt="${episodeName}">
+      <p>${episodeSummary}</p>
+    `;
+
+    rootDiv.appendChild(listItem);
+  });
 }
 
 window.onload = setup;
