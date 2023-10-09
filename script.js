1c29454dfc4194fb5a8e97c7ada2ea4b1c74c364
Denis Dudar <denis.dudar11@gmail.com>
2023-10-09 17:17:30 +0300

    Merge feature/level-100 into for-review

--- script.js
+++ script.js
@@ -6,7 +6,25 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  return episodeList.map(episode => {
+    const div = document.createElement("div");
+    div.classList.add("episode");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = `${episode.name} - ${episode.number < 10
+      ? "S0"+episode.season+"E0"+episode.number 
+      : "S0"+episode.season+"E"+episode.number}`;
+
+    const img = document.createElement("img");
+    img.src = episode.image.medium;
+
+    const p = document.createElement("p");
+    p.textContent = episode.summary;
+
+    div.append(h3, img, p);
+    rootElem.appendChild(div);
+  })
 }
 
 window.onload = setup;
