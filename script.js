2d39a57a2777b1d4806d0ed162fe8bb2c7510729
oleks211 <aleksandr.savelyev@gmail.com>
2023-10-11 23:32:57 +0300

    complete level-350

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //You can edit ALL of the code here
 const rootDiv = document.getElementById("root");
-const episodes = getAllEpisodes();
-var filteredEpisodes = episodes;
+let episodes = [];
+let filteredEpisodes = [];
 const searchInput = document.getElementById("search");
 const select = document.getElementById("select");
+const allValue = 'All';
 
 function formatEpisodeCode(season, episode) {
   return `S${season.toString().padStart(2, "0")}E${episode
@@ -37,9 +38,10 @@ function showEpisodesCount(episodes, filtered) {
 
 function fillSelect() {
   let option = document.createElement("option");
-  option.text = 'All';
-  option.value = 'All';
+  option.text = allValue;
+  option.value = allValue;
   select.add(option);
+
   episodes.map((episode) => {
     let option = document.createElement("option");
     let episodeCode = formatEpisodeCode(episode.season, episode.number);
@@ -62,11 +64,11 @@ searchInput.addEventListener("input", function () {
   showEpisodesCount(episodes, filteredEpisodes);
 });
 
-select.addEventListener("change", function() {
+select.addEventListener("change", function () {
   var selectedOption = select.options[select.selectedIndex];
   var targetSectionId = selectedOption.value;
 
-  if (selectedOption.value === 'All') {
+  if (selectedOption.value === allValue) {
     filteredEpisodes = episodes;
   } else {
     filteredEpisodes = episodes.filter((episode) => {
@@ -84,4 +86,27 @@ function preparePage() {
   fillSelect();
 }
 
-window.onload = preparePage;
+let url = "https://api.tvmaze.com/shows/82/episodes";
+// let url = "https://api.tvmaze.com/shows/527/episodes";
+// let url = "https://api.tvmaze.com/shows/22036/episodes";
+// let url = "https://api.tvmaze.com/shows/5/episodes";
+// let url = "https://api.tvmaze.com/shows/582/episodes"; // did not work
+// let url = "https://api.tvmaze.com/shows/179/episodes";
+// let url = "https://api.tvmaze.com/shows/379/episodes";
+// let url = "https://api.tvmaze.com/shows/4729/episodes"; // did not work
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network problems");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    episodes = data;
+    filteredEpisodes = episodes;
+    preparePage();
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
