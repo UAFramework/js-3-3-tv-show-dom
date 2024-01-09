e1a9dc3d0ccbd2b19dc2aa8ff43a304fa4e3821f
66Irenka <pavlenkoiren66@gmail.com>
2024-01-10 01:48:20 +0200

    complene level - 100

--- script.js
+++ script.js
@@ -6,7 +6,30 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+
+  episodeList.forEach(displayEpisode);
+}
+
+function displayEpisode(episode) {
+  const { season, number, name, image, summary, id } = episode;
+
+  const formattedSeason = season.toString().padStart(2, "0");
+  const formattedEpisode = number.toString().padStart(2, "0");
+  const episodeCode = `S${formattedSeason}E${formattedEpisode}`;
+
+  const listItem = document.createElement("li");
+  listItem.className = "episode-item";
+
+  listItem.innerHTML = `
+    <h3 class="episode-title">${episodeCode}: ${name}</h3>
+    <img class="episode-image" src="${image.medium}" alt="${name}">
+    <p class="episode-summary">${summary}</p>
+    <p class="source-link">Data sourced from <a href="${id}" target="_blank">TVMaze.com</a></p>
+  `;
+
+  const rootElem = document.getElementById("root");
+  rootElem.appendChild(listItem);
 }
 
 window.onload = setup;
